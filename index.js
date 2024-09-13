923524ccbd648eb55858803bfe496fcb630d485c
CaioTarso <caiotarso7@gmail.com>
2024-09-13 14:41:29 -0300

    fundamentos01

---
+++ index.js
@@ -0,0 +1,19 @@
+//array, objetos
+let meta = {
+    value: "Aprender Node",
+    cheked: true
+}
+
+let metas = [
+    meta,
+    {
+       value: "caminhar 20 minutos todos os dias",
+       cheked: false
+    }
+]
+
+console.log(metas[1].value)
+//function // arrow function
+const criarMeta = () => {
+    
+}
